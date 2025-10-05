736f35906840e70428d66e4ff26916b3a2d85da9
feelframestudio <feelframestudio@gmail.com>
2025-10-05 16:19:18 +0100

    Delete feelframestudio directory

--- feelframestudio/app/works/[videoId]/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import Video from './Video';
-
-export default async function VideoPage(props: { params: Promise<{ videoId: string }> }) {
-  const { videoId } = await props.params; // ✅ await before accessing
-  return <Video videoId={videoId} />;
-}
